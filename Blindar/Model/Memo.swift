7a609d7fc2b43bfb36e4812f783e6a83dfefa4b5
sujileelea <sujileelea@gmail.com>
2024-06-27 14:36:02 +0900

    change struct Shcool's property name shcoolId - > school_code

--- Blindar/Model/Memo.swift
+++ Blindar/Model/Memo.swift
@@ -12,5 +12,4 @@ struct Memo {
     var date: String //yyyyMMdd
     var memo_id: String //메모 id
     var contents: String //수정할 일정 내용
-    var message: String //API 응답 메세지
 }
--- Blindar/Model/School.swift
+++ Blindar/Model/School.swift
@@ -9,5 +9,5 @@ import Foundation
 
 struct School {
     var school_name: String //학교 이름 (한글)
-    var schoolId: String //NEIS API에서 제공하는 표준 학교 코드
+    var school_code: String //NEIS API에서 제공하는 표준 학교 코드
 }
