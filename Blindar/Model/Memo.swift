6889afd3c3116e409edef2e78d8914fafaf81ca6
sujileelea <sujileelea@gmail.com>
2024-07-16 15:53:25 +0900

    add coding keys for Memo Model

--- Blindar/Model/Memo.swift
+++ Blindar/Model/Memo.swift
@@ -9,10 +9,17 @@ import Foundation
 import SwiftData
 
 struct Memo: Codable {
-    var user_id: String //사용자 UID
+    var userId: String //사용자 UID
     var date: String //yyyyMMdd
-    var memo_id: String //메모 id
+    var memoId: String //메모 id
     var contents: String //수정할 일정 내용
+    
+    enum CodingKeys: String, CodingKey {
+        case userId = "user_id"
+        case memoId = "memo_id"
+        case date
+        case contents
+    }
 }
 
 //@Model
