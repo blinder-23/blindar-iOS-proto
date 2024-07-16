86883656289ff2f50ce1712bdce0778e4449dec4
sujileelea <sujileelea@gmail.com>
2024-07-16 18:06:15 +0900

    add coding keys for Feedback Model

--- Blindar/Model/Feedback.swift
+++ Blindar/Model/Feedback.swift
@@ -8,9 +8,17 @@
 import Foundation
 
 struct Feedback: Codable {
-    var user_id: String //사용자 id
-    var device_name: String //기기 이름 ex.SM-F731
-    var os_version: String  //OS version
-    var app_version: String  //App version
+    var userId: String //사용자 id
+    var deviceName: String //기기 이름 ex.SM-F731
+    var osVersion: String  //OS version
+    var appVersion: String  //App version
     var contents: String //피드백 내용
+    
+    enum CodingKeys: String, CodingKey {
+        case userId = "user_id"
+        case deviceName = "device_name"
+        case osVersion = "os_version"
+        case appVersion = "app_version"
+        case contents
+    }
 }
